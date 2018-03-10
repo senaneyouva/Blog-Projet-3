16481bce494712c9de9fc181a66faf04f5440ab6
Youva Senane <yuvasenane@hotmail.com>
2018-03-10 15:56:13 +0100

    Update prod.php

--- app/config/prod.php
+++ app/config/prod.php
@@ -4,11 +4,11 @@
 $app['db.options'] = array(
     'driver'   => 'pdo_mysql',
     'charset'  => 'utf8',
-    'host'     => 'db688128399.db.1and1.com',
-    'port'     => '3306',
-    'dbname'   => 'db688128399',
-    'user'     => 'dbo688128399',
-    'password' => '22124215',
+    'host'     => '',
+    'port'     => '',
+    'dbname'   => '',
+    'user'     => '',
+    'password' => '',
 );
 
 // define log parameters
